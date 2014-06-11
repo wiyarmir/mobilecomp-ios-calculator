e49b0519301b1735284d7f1e9cc111acef44387c
Guillermo Orellana <wiyarmir@gmail.com>
2014-06-11 18:23:41 +0200

    Initial Commit

---
+++ Calculator/CalcAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalcAppDelegate.h
+//  Calculator
+//
+//  Created by Guillermo Orellana on 11/06/14.
+//  Copyright (c) 2014 Guillermo Orellana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalcAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalcViewController.m
@@ -0,0 +1,29 @@
+//
+//  CalcViewController.m
+//  Calculator
+//
+//  Created by Guillermo Orellana on 11/06/14.
+//  Copyright (c) 2014 Guillermo Orellana. All rights reserved.
+//
+
+#import "CalcViewController.h"
+
+@interface CalcViewController ()
+
+@end
+
+@implementation CalcViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
