89756bdd878936ee26e058a39e8f70a4068dac04
Guillermo Orellana <wiyarmir@gmail.com>
2014-06-12 20:58:19 +0200

    works

--- Calculator/CalcViewController.h
+++ Calculator/CalcViewController.h
@@ -10,4 +10,29 @@
 
 @interface CalcViewController : UIViewController
 
+- (IBAction)zero:(id)sender;
+- (IBAction)one:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+- (IBAction)clear:(id)sender;
+- (IBAction)dot:(id)sender;
+- (IBAction)backspace:(id)sender;
+- (IBAction)eq:(id)sender;
+- (IBAction)plus:(id)sender;
+- (IBAction)minus:(id)sender;
+- (IBAction)times:(id)sender;
+- (IBAction)fraction:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *display;
+@property (weak, nonatomic) IBOutlet UILabel *opdisplay;
+
+typedef enum{
+    EQ, ADD,SUB,MUL,DIV
+} OpsType;
+
 @end
--- Calculator/CalcViewController.m
+++ Calculator/CalcViewController.m
@@ -8,7 +8,11 @@
 
 #import "CalcViewController.h"
 
-@interface CalcViewController ()
+@interface CalcViewController () {
+    NSString *stack;
+    double result;
+    OpsType lastop;
+}
 
 @end
 
@@ -18,6 +22,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self clear:0];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +30,157 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)zero:(id)sender {
+    [self addnumber: 0];
+}
+
+- (IBAction)one:(id)sender {
+    [self addnumber: 1];
+}
+
+- (IBAction)two:(id)sender {
+    [self addnumber: 2];
+}
+
+- (IBAction)three:(id)sender {
+    [self addnumber: 3];
+}
+
+- (IBAction)four:(id)sender {
+    [self addnumber: 4];
+}
+
+- (IBAction)five:(id)sender {
+    [self addnumber: 5];
+}
+
+- (IBAction)six:(id)sender {
+    [self addnumber: 6];
+}
+
+- (IBAction)seven:(id)sender {
+    [self addnumber: 7];
+}
+
+- (IBAction)eight:(id)sender {
+    [self addnumber: 8];
+}
+
+- (IBAction)nine:(id)sender {
+    [self addnumber: 9];
+}
+
+- (IBAction)clear:(id)sender {
+    stack = @"0";
+    result = 0.0;
+    [_display setText:stack];
+}
+
+- (IBAction)dot:(id)sender {
+    [self adddot];
+}
+
+- (IBAction)backspace:(id)sender {
+    [self addnumber: -1];
+}
+
+- (IBAction)eq:(id)sender {
+    [self logic: EQ];
+}
+
+- (IBAction)plus:(id)sender {
+    [self logic: ADD];
+}
+
+- (IBAction)minus:(id)sender {
+    [self logic: SUB];
+}
+
+- (IBAction)times:(id)sender {
+    [self logic: MUL];
+}
+
+- (IBAction)fraction:(id)sender {
+    [self logic: DIV];
+}
+
+-(void)addnumber:(int)number{
+    
+    if(stack == NULL){
+        stack = @"0";
+    }
+    
+    if(number > -1){
+        if (![stack compare:@"0" ] ) {
+            stack = [NSString stringWithFormat:@"%1$d", number];
+        }else{
+            stack = [NSString stringWithFormat:@"%1$@%2$d", stack, number];
+        }
+    }else if([stack length] > 0){
+        stack = [stack substringToIndex:[stack length] + number];
+    }
+    
+    if([stack length] <= 0){
+        stack = @"";
+    }
+    
+    [_display setText:stack];
+}
+
+-(void)adddot{
+    stack = [NSString stringWithFormat:@"%1$@.", stack];
+    [_display setText:stack];
+}
+
+
+-(void)logic:(OpsType)type{
+    if(type == EQ){
+        switch (lastop) {
+            case ADD:
+                stack = [NSString stringWithFormat:@"%f", [stack doubleValue] + result];
+                break;
+                
+            case SUB:
+                stack = [NSString stringWithFormat:@"%f", result - [stack doubleValue]];
+                break;
+                
+            case MUL:
+                stack = [NSString stringWithFormat:@"%f", [stack doubleValue] * result];
+                break;
+                
+            case DIV:
+                stack = [NSString stringWithFormat:@"%f", result / [stack doubleValue]];
+                break;
+            default:
+                break;
+        }
+        [_opdisplay setText:@"="];
+        
+    }else{
+        switch (type) {
+            case ADD:
+                [_opdisplay setText:@" "];
+                break;
+            case SUB:
+                [_opdisplay setText:@"-"];
+                break;
+            case MUL:
+                [_opdisplay setText:@"x"];
+                break;
+            case DIV:
+                [_opdisplay setText:@"/"];
+                break;
+                
+            default:
+                break;
+        }
+        lastop = type;
+        result = [stack doubleValue];
+        stack = @"0";
+    }
+    [_display setText: stack];
+}
+
 
 @end
